cb3cf7a0147968a5cfa8975ebdadbe29c1244f3c
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-02-04 17:05:49 +0300

    Remove duplicates

--- plugins/ontarget/catalog/updates/SeedRemoveDuplicatePropertyValues.php
+++ plugins/ontarget/catalog/updates/SeedRemoveDuplicatePropertyValues.php
@@ -15,32 +15,35 @@ class SeedRemoveDuplicatePropertyValues extends Seeder
     public function run()
     {
         try {
-            trace_log('Start seeding');
-            $duplicates = PropertyValue::query()
-                                       ->select('property_id', 'name', DB::raw('MIN(id) as min_id'))
-                                       ->groupBy('property_id', 'name')
-                                       ->havingRaw('COUNT(*) > 1')
-                                       ->get();
+            DB::transaction(function () {
+                $duplicates = PropertyValue::query()
+                                           ->select('property_id', 'name', DB::raw('MIN(id) as min_id'))
+                                           ->groupBy('property_id', 'name')
+                                           ->havingRaw('COUNT(*) > 1')
+                                           ->get();
 
-            $usedPropertyValueIds = DB::table('ontarget_catalog_product_property_value')
-                                      ->pluck('property_value_id')
-                                      ->toArray();
+                $usedPropertyValueIds = DB::table('ontarget_catalog_product_property_value')
+                                          ->pluck('property_value_id')
+                                          ->toArray();
 
-            foreach ($duplicates as $duplicate) {
-                $idsToDelete = PropertyValue::query()
-                                            ->where('property_id', $duplicate->property_id)
-                                            ->where('name', $duplicate->name)
-                                            ->whereNotIn('id', $usedPropertyValueIds)
-                                            ->where('id', '!=', $duplicate->min_id)
-                                            ->pluck('id')
-                                            ->toArray();
+                foreach ($duplicates as $duplicate) {
+                    $idsToDelete = PropertyValue::query()
+                                                ->where('property_id', $duplicate->property_id)
+                                                ->where('name', $duplicate->name)
+                                                ->whereNotIn('id', $usedPropertyValueIds)
+                                                ->where('id', '!=', $duplicate->min_id)
+                                                ->pluck('id')
+                                                ->toArray();
 
-                if (!empty($idsToDelete)) {
-                    PropertyValue::query()
-                                 ->whereIn('id', $idsToDelete)
-                                 ->delete();
+                    if (!empty($idsToDelete)) {
+                        foreach (array_chunk($idsToDelete, 1000) as $chunk) {
+                            PropertyValue::query()
+                                         ->whereIn('id', $chunk)
+                                         ->delete();
+                        }
+                    }
                 }
-            }
+            });
         } catch (\Throwable $exception){
             trace_log($exception);
         }
