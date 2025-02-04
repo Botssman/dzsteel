077026cd3f763f2b17b725a7ed505715d8945961
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-02-04 16:09:17 +0300

    Remove duplicates

---
+++ plugins/ontarget/catalog/updates/seed_remove_duplicate_property_values.php
@@ -0,0 +1,46 @@
+<?php namespace OnTarget\Catalog\Updates;
+
+use Db;
+use OnTarget\Catalog\Models\PropertyValue;
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * ontarget_catalog_category_property Migration
+ *
+ * @link https://docs.octobercms.com/3.x/extend/database/structure.html
+ */
+class SeedRemoveDuplicatePropertyValues extends \Seeder
+{
+    public function run()
+    {
+        DB::transaction(function () {
+            $duplicates = PropertyValue::query()
+                                       ->select('property_id', 'name', DB::raw('MIN(id) as min_id'))
+                                       ->groupBy('property_id', 'name')
+                                       ->havingRaw('COUNT(*) > 1')
+                                       ->get();
+
+            $usedPropertyValueIds = DB::table('ontarget_catalog_product_property_value')
+                                      ->pluck('property_value_id')
+                                      ->toArray();
+
+            foreach ($duplicates as $duplicate) {
+                $idsToDelete = PropertyValue::query()
+                                            ->where('property_id', $duplicate->property_id)
+                                            ->where('name', $duplicate->name)
+                                            ->whereNotIn('id', $usedPropertyValueIds)
+                                            ->where('id', '!=', $duplicate->min_id) // Оставляем одну запись
+                                            ->pluck('id')
+                                            ->toArray();
+
+                if (!empty($idsToDelete)) {
+                    PropertyValue::query()
+                                 ->whereIn('id', $idsToDelete)
+                                 ->delete();
+                }
+            }
+        });
+    }
+}
