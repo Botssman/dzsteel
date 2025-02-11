acd9ba1791ea33afbc71b35cdab1d05be03f902f
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-02-11 13:12:53 +0300

    job

--- plugins/ontarget/catalog/classes/jobs/DeleteDuplicatesJob.php
+++ plugins/ontarget/catalog/classes/jobs/DeleteDuplicatesJob.php
@@ -8,28 +8,32 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Jobs\Job;
 use OnTarget\Catalog\Models\PropertyValue;
 
-class DeleteDuplicatesJob implements ShouldQueue
-{
-    use Queueable;
-    use InteractsWithQueue;
-
-    public $duplicate;
-
-
+class DeleteDuplicatesJob {
     public function fire(Job $job, array $data)
     {
         try {
+
+            // Находим оригинальную запись
             $original = PropertyValue::find($data['duplicate']['original_id']);
             if (!$original) return;
+
+            // Находим все дубликаты для этого slug
             $duplicateValues = PropertyValue::where('slug', 'like', $original->slug . '%')
                                             ->where('id', '!=', $original->id)
                                             ->get();
 
+            // Переносим связи с Product на оригинальную запись
             foreach ($duplicateValues as $duplicateValue) {
+                // Получаем все продукты, связанные с дубликатом
                 $products = $duplicateValue->products;
+
+                // Привязываем эти продукты к оригинальной записи
                 $original->products()->syncWithoutDetaching($products->pluck('id')->toArray());
+
+                // Удаляем дубликат
                 $duplicateValue->delete();
             }
+
         } catch (\Throwable $exception) {
             trace_log($exception->getMessage());
         }
