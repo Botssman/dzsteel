2904981aac9ac89ba2218c26ad50948710d5b625
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-02-11 13:38:24 +0300

    job

--- plugins/ontarget/catalog/routes.php
+++ plugins/ontarget/catalog/routes.php
@@ -4,12 +4,12 @@ use OnTarget\Catalog\Models\PropertyValue;
 
 \Route::get('dup', function(){
 
-$duplicates = PropertyValue::query()
-    ->select('slug')
-    ->selectRaw('MIN(id) as original_id')
-    ->groupBy('slug')
-    ->havingRaw('COUNT(*) > 1')
-    ->get();
+    $duplicates = PropertyValue::query()
+                               ->selectRaw('MIN(slug) as slug') // Используем MIN(slug) для выбора одного значения
+                               ->selectRaw('MIN(id) as original_id')
+                               ->groupBy(\DB::raw("REGEXP_REPLACE(slug, '-\\d+$', '')"))
+                               ->havingRaw('COUNT(*) > 1')
+                               ->get();
 
 foreach($duplicates as $duplicate){
 \Queue::push(OnTarget\Catalog\Classes\Jobs\DeleteDuplicatesJob::class,['duplicate'=>$duplicate]);
