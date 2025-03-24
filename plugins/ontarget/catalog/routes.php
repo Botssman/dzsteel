1da6d21cc2e61c7f7697ae3a107dfe40e72c138e
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-03-24 22:52:12 +0300

    dup

--- plugins/ontarget/catalog/routes.php
+++ plugins/ontarget/catalog/routes.php
@@ -1,20 +1,25 @@
 <?php
-
 use OnTarget\Catalog\Models\PropertyValue;
 
 \Route::get('dup', function(){
 
     $duplicates = PropertyValue::query()
-                               ->selectRaw('original_slug') // Используем MIN(slug) для выбора одного значения
-                               ->selectRaw('MIN(id) as original_id')
-                               ->groupBy('original_slug')
-                               ->havingRaw('COUNT(*) > 1')
+                               ->select('original_slug', 'property_id') // Группируем по original_slug и property_id
+                               ->selectRaw('MIN(id) as original_id') // Выбираем минимальный id в группе
+                               ->groupBy('original_slug', 'property_id') // Группируем по original_slug и property_id
+                               ->havingRaw('COUNT(*) > 1') // Оставляем только группы с дубликатами
                                ->get();
 
-//foreach($duplicates as $duplicate){
-\Queue::push(OnTarget\Catalog\Classes\Jobs\DeleteDuplicatesJob::class,['duplicate'=>$duplicate]);
-//}
+    foreach($duplicates as $duplicate){
+        \Queue::push(OnTarget\Catalog\Classes\Jobs\DeleteDuplicatesJob::class,['duplicate'=>$duplicate]);
+    }
 
     dd($duplicates->toArray());
 
 });
+
+
+
+
+
+
