2345cd54d6bcec43e39ef023f60cd9ba3e2d07d9
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-03-24 22:50:46 +0300

    Indexes to property values

---
+++ plugins/ontarget/catalog/updates/add_indexes_to_property_values_table.php
@@ -0,0 +1,26 @@
+<?php namespace OnTarget\Catalog\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('ontarget_catalog_property_values', function(Blueprint $table){
+            $table->index('slug');
+            $table->index('name');
+            $table->unique(['property_id', 'name']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('ontarget_catalog_property_values', function(Blueprint $table){
+            $table->dropUnique(['property_id', 'name']);
+            $table->dropIndex(['slug']);
+            $table->dropIndex(['name']);
+        });
+    }
+};
