25d8f9fe1037ecfa348f2518930caa1bfc669089
Dmitry Ivanchenko <dmi3mart@ya.ru>
2025-03-24 23:44:49 +0300

    job fix

--- plugins/ontarget/catalog/classes/jobs/DeleteDuplicatesJob.php
+++ plugins/ontarget/catalog/classes/jobs/DeleteDuplicatesJob.php
@@ -2,41 +2,42 @@
 
 namespace OnTarget\Catalog\Classes\Jobs;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Jobs\Job;
 use OnTarget\Catalog\Models\PropertyValue;
 
 class DeleteDuplicatesJob {
-    public $timeout = 600;
+    public $timeout = 3600;
 
-    public $tries = 3;
-
-    public $maxExceptions = 1;
+    public $tries = 5;
 
     public function fire(Job $job, array $data)
     {
         try {
+
             // Находим оригинальную запись
             $original = PropertyValue::find($data['duplicate']['original_id']);
             if (!$original) return;
 
             // Находим все дубликаты для этого slug
             $duplicateValues = PropertyValue::where('slug', 'like', $original->slug . '-%')
-            ->where('id', '!=', $original->id)
-            ->orderBy('id')
-            ->get();
+                                            ->where('id', '!=', $original->id)
+                                            ->get();
 
-            trace_log($duplicateValues->count());
-
+            // Переносим связи с Product на оригинальную запись
             foreach ($duplicateValues as $duplicateValue) {
+
                 \Queue::push(
                     ProcessSingleDuplicateJob::class,
                     [
                         'duplicate_id' => $duplicateValue->id,
-                        'original_id' => $original->id,
+                        'original_id' => $original->id
                     ]
                 );
-            }
 
+            }
 
         } catch (\Throwable $exception) {
             trace_log($exception->getMessage());
