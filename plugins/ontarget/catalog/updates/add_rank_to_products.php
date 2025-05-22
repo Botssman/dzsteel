0efe3c2592a97054f1fd7c2dbe029216bae8a652
Dmitry I <dmi3mart@ya.ru>
2025-05-22 16:19:13 +0300

    migration fix

--- plugins/ontarget/catalog/updates/add_rank_to_products.php
+++ plugins/ontarget/catalog/updates/add_rank_to_products.php
@@ -1,8 +1,8 @@
-<?php
+<?php namespace OnTarget\Catalog\Updates;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
 
 return new class extends Migration {
     public function up()
